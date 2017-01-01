dac97c4b5450ec9ccf2c8dbc50048dd08012c89b
Andrey Pugachenko <andrqxa@gmail.com>
2017-01-02 00:10:31 +0300

    Added chapter2 application for geometry.

---
+++ 02_Chapter02_Geometry/src/geometry.pl
@@ -0,0 +1,9 @@
+point(X, Y).
+
+segment(point(X1, Y1), point(X2, Y2)).
+
+segment(point(X1, Y1), point(X2, Y2), point(X3, Y3)).
+
+vertical_segment(point(X1, Y1), point(X1, Y2)).
+
+horizontal_segment(point(X1, Y1), point(X2, Y1)).
