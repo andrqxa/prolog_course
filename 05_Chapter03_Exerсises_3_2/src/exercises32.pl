f19d4d368f98c69b962b8027061ea0c2d534229c
Andrey Pugachenko <andrqxa@gmail.com>
2017-01-10 00:30:54 +0300

    Added exercises

--- 05_Chapter03_Exerсises_3_2/src/exercises32.pl
+++ 05_Chapter03_Exerсises_3_2/src/exercises32.pl
@@ -31,7 +31,46 @@ reverse([], []).
 reverse([X|Y], Z) :-
 	reverse(Y, Y1),
 	conc(Y1, [X], Z).
+	
+% Last element of list
+last1(X, L) :-
+	conc(_L1, [X], L).
+	
+% Palindrome
+palindrome([]).
 
-palindrome([], []).
+palindrome([_]).
 
-palindrome([X | Y], ).
+palindrome([X|Y]) :-
+	conc(Z, [X], Y),
+	palindrome(Z).
+	
+% Palindrome via reverse
+palindrome1(X) :-
+	reverse(X, X).
+	
+% Shilt list to left by one element
+shift([], []).
+
+shift([X|Y], L) :-
+	conc(Y, [X], L).
+
+% Translate numbers to words
+means(0, zero).
+means(1, one).
+means(2, two).
+means(3, three).
+means(4, four).
+means(5, five).
+means(6, six).
+means(7, seven).
+means(8, eight).
+means(9, nine).
+
+translate([], []).
+
+translate([X|Y], L) :-	
+	translate(Y, Y1),
+	means(X, W),
+	conc([W], Y1, L).
+	
