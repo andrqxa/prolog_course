321a3723338d0e8d34dae2435c9d2a6160979a4c
Andrii Puhachenko <andrqxa@gmail.com>
2024-05-02 11:56:18 +0200

    Made makefiles for mercury

--- 01_Chapter01_Family/src/Family.m
+++ 01_Chapter01_Family/src/Family.m
@@ -1,40 +1,71 @@
-%===============================================================================
+%-----------------------------------------------------------------------------%
+% vim: ft=mercury ts=4 sw=4 et
+%-----------------------------------------------------------------------------%
+% Copyright (C) 2024 Andrii Puhachenko.
+% See the file COPYING for license details.
+%-----------------------------------------------------------------------------%
+
+:- module family.
+:- interface.
+
+:- import_module io.
+
+%-----------------------------------------------------------------------------%
+
+:- pred main(io::di, io::uo) is det.
+
+:- pred parent(string::in, string::in) is semidet.
+:- pred woman(string::in) is semidet.
+:- pred man(string::in) is semidet.
+:- pred child(string::in, string::in) is semidet.
+:- pred mother(string::in, string::in) is semidet.
+:- pred parent_of_parent(string::in, string::in) is semidet.
+:- pred sister(string::in, string::in) is semidet.
+:- pred happy(string::in) is semidet.
+:- pred has_2_child(string::in) is semidet.
+:- pred grandson(string::in, string::in) is semidet.
+:- pred aunt(string::in, string::in) is semidet.
+
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
+
+:- implementation.
+
+%-----------------------------------------------------------------------------%
+
 % Family facts
-%===============================================================================
-parent(pam, bob).
-parent(tom, bob).
-parent(tom, liz).
-parent(bob, ann).
-parent(bob, pat).
-parent(pat, jim).
+parent("pam", "bob").
+parent("tom", "bob").
+parent("tom", "liz").
+parent("bob", "ann").
+parent("bob", "pat").
+parent("pat", "jim").
 
-woman(pam).
-woman(liz).
-woman(pat).
-woman(ann).
-man(jim).
-man(tom).
-man(bob).
+woman("pam").
+woman("liz").
+woman("pat").
+woman("ann").
+man("jim").
+man("tom").
+man("bob").
 
-%===============================================================================
 % Family rules
-%===============================================================================
-child(X, Y) :- 
+child(X, Y) :-
 	parent(Y, X).
 
-mother(X, Y) :-	
+mother(X, Y) :-
 	parent(X, Y),
 	woman(X).
-	
+
 parent_of_parent(X, Y) :-
 	parent(X, Z),
 	parent(Z, Y).
 
-sister(X, Y) :-	
+sister(X, Y) :-
 	parent(Z, X),
 	parent(Z, Y),
 	woman(X).
-	
+
 happy(X) :-
 	parent(X, _).
 
@@ -50,3 +81,20 @@ grandson(X, Y) :-
 aunt(X, Y) :-
 	parent(Z, Y),
 	sister(X, Z).
+
+%-----------------------------------------------------------------------------%
+
+% Main function for running
+main(!IO) :-
+	io.write_string("Family rules in Mercury", !IO).
+
+%-----------------------------------------------------------------------------%
+:- end_module family.
+%-----------------------------------------------------------------------------%
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -6,6 +6,33 @@ branch `mercury` is current
 
 branch `master` is clean
 
+# License
+repository is licensed under a simple 2-clause BSD style license. See the file COPYING for details.
+
+# Installation
+Check the values in the file Make.options to see if they agree with your system, then do:
+    - Compiler is `mercury.bat` on Windows and `mmc` on Linux (fix in Make.options)
+    - PROGRAM_EXT = .exe have to be fix (empty) for Linux
+    - It's better to compile in `GitBash` on Windows, otherwise you must fix the goals `install` and `clean` in src/Makefile
+
+```bash
+    $ make install
+```
+You can also override values in Make.options on the command line, for example
+
+```bash
+    $ make INSTALL_PREFIX=/foo/bar install
+```
+causes the library to be installed in the directory /foo/bar.
+
+# Mercury Compatibility
+The code on the mercury branch was compiled with  Mercury ROTD 2024-04-26
+
+# Author
+Andrii Puhachenko andrqxa@gmail.com
+
+# Contents
+
 ## Part I. Prolog language.
 
 ### Chapter 1.
